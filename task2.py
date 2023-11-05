3ea72c9542e8886ad5c1e5ec78964b00521cabd0
Vaishnavi Avhad <98690195+vaishnaviavhad2511@users.noreply.github.com>
2023-11-05 20:17:05 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,51 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
+
+while True:
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+        continue
+
+    print("Choose an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Quit")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice == '5':
+        print("Calculator is exiting. Goodbye!")
+        break
+    elif choice in ('1', '2', '3', '4'):
+        if choice == '1':
+            result = add(num1, num2)
+            operator = '+'
+        elif choice == '2':
+            result = subtract(num1, num2)
+            operator = '-'
+        elif choice == '3':
+            result = multiply(num1, num2)
+            operator = '*'
+        elif choice == '4':
+            result = divide(num1, num2)
+            operator = '/'
+
+        print(f"{num1} {operator} {num2} = {result}")
+    else:
+        print("Invalid input. Please choose a valid operation (1/2/3/4).")
